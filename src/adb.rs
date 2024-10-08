242b0cb920cbea108d786dbf5682990e526909d9
Troy Takac <thefennecdeer@gmail.com>
2024-10-08 13:50:42 +1100

    Refactor: ADB Struct

--- src/adb.rs
+++ src/adb.rs
@@ -1,46 +1,69 @@
 use core::str;
 use std::process::Stdio;
+use std::env::current_dir;
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Ok};
 use tokio::process::Command;
 
-pub async fn check_for_adb() -> Result<(), anyhow::Error> {
+#[derive(Debug)]
+pub struct Manager {
+    path: String,
+    command: Command
+}
 
-    const ADB_VERSION_RESULT: &str = "Android Debug Bridge version";
-
-    let mut cmd = Command::new("adbb");
-
-    cmd.arg("version");
-
-    // Specify that we want the command's standard output piped back to us.
-    // By default, standard input/output/error will be inherited from the
-    // current process (for example, this means that standard input will
-    // come from the keyboard and standard output/error will go directly to
-    // the terminal if this process is invoked from the command line).
-    
-    cmd.stdout(Stdio::piped());
-
-    let child = cmd.spawn();
-    match child {
-        Ok(_) => {
-            let op = child?.wait_with_output().await?;
-            let op_utf = str::from_utf8(&op.stdout).unwrap();
-            println!("{}", op_utf);
-            if op_utf.contains(ADB_VERSION_RESULT) {
-                Ok(())
-            }
-            else {
-                Err(anyhow!("Something Strange!"))
-            }
+impl Manager {
+    pub fn new() -> Self {
+        Self {
+            path: String::new(),
+            command: Command::new("adb")
         }
-        Err(_e) => {
-            Err(anyhow!("ADB Tools Not Found!"))
+    }
+    pub async fn check_for_adb(&mut self) -> Result<(), anyhow::Error> {
+        let mut adb_dir = current_dir()
+                    .expect("Failed to get current working directory");
+        adb_dir.extend(&["platform-tools", "adb"]);
+        let mut cmd_system = Command::new("adb");
+        let mut cmd_local = Command::new(&adb_dir);
+
+        if Manager::check_for_adb_file(&mut cmd_local).await.unwrap() {
+            self.path = adb_dir.into_os_string().into_string().unwrap();
+            self.command = cmd_local;
+            Ok(())
+
+        }
+        else if Manager::check_for_adb_file(&mut cmd_system).await.unwrap() {
+            self.path = "adb".to_string();
+            self.command = cmd_system;
+            Ok(())
+        }
+        else {
+            Err(anyhow!("NAH"))
+
         }
     }
 
+    async fn check_for_adb_file(cmd: &mut Command) -> Result<bool, anyhow::Error> {
 
-    
-    
-    
+        const ADB_VERSION_RESULT: &str = "Android Debug Bridge version";
+        cmd.arg("version");
+
+        cmd.stdout(Stdio::piped());
+
+        let child= cmd.spawn();
+        match child {
+            Result::Ok(_) => {
+                let op = child?.wait_with_output().await?;
+                let op_utf = str::from_utf8(&op.stdout).unwrap();
+                if op_utf.contains(ADB_VERSION_RESULT) {
+                    Ok(true)
+                }
+                else {
+                    Err(anyhow!("Something Strange!"))
+                }
+            }
+            Err(_e) => {
+                Ok(false)
+            }
+        }
+    }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use console::Style;
+use anyhow::anyhow;
+use console::{style, Style};
 use dialoguer::{theme::ColorfulTheme, Confirm, Input};
 use std::env::current_dir;
-use quest2deployer::{io,zip,adb};
+use quest2deployer::{adb::{self, Manager}, io, zip};
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -34,55 +35,56 @@ fn init_config() -> Result<Option<Config>, anyhow::Error> {
     }))
 }
 
-async fn setup_adbtools() -> Result<bool, anyhow::Error> {
+async fn setup_adbtools(adb_manager : &mut Manager) -> Result<(), anyhow::Error> {
     let theme = ColorfulTheme {
         values_style: Style::new().yellow().dim(),
         ..ColorfulTheme::default()
     };
 
-    let adb_result = adb::check_for_adb().await;
+    let adb_result = adb_manager.check_for_adb().await;
     match adb_result {
         Ok(_) => {
-            
+         return Ok(())  
         }
         Err(_e) => {
-           
+            println!(
+                "ADB TOOLS NOT FOUND!: {:010x}",
+                style(42).red().on_black().bold()
+            );
+            let confirmation = Confirm::with_theme(&theme)
+            .with_prompt("Download ADB Tools Now?")
+            .interact()
+            .unwrap();
+        
+            if confirmation {
+                println!("Looks like you want to continue");
+            } else {
+                return Err(anyhow!("Escape!"));
+            };
+            
+            let client = reqwest::Client::new();
+        
+            let mut out_dir = current_dir()
+                .expect("Failed to get current working directory");
+        
+            let downloaded_zip = io::download_url(&client,"https://dl.google.com/android/repository/platform-tools-latest-darwin.zip", "adbtools.zip").await.unwrap();
+            zip::unzip_file(downloaded_zip, &out_dir).await;
+            out_dir.extend(&["platform-tools", "adb"]);
+        
+            Ok(())
         }
     }
-
-    let confirmation = Confirm::with_theme(&theme)
-        .with_prompt("Do you want to continue?")
-        .interact()
-        .unwrap();
     
-    if confirmation {
-        println!("Looks like you want to continue");
-    } else {
-        println!("nevermind then :(");
-        return Ok(false);
-    };
-    let client = reqwest::Client::new();
-
-    let out_dir = current_dir()
-        .expect("Failed to get current working directory");
-
-    let downloaded_zip = io::download_url(&client,"https://dl.google.com/android/repository/platform-tools-latest-darwin.zip", "adbtools.zip").await.unwrap();
-
-    zip::unzip_file(downloaded_zip, &out_dir).await;
-
-    Ok(true)
 }
 
 #[tokio::main]
 async fn main() {
-    match setup_adbtools().await {
-        Ok(true) => println!("continue!"),
-        Ok(false) => {
-            println!("Aborted.");
-            return;
-        },
-        Err(err) => println!("error: {}", err),
+    let mut adb_manager = adb::Manager::new();
+    match setup_adbtools(&mut adb_manager).await {
+        Ok(_) => println!("Yeha"),
+        Err(_err) => return,
     }
+    println!("{:?}", adb_manager);
     match init_config() {
         Ok(None) => println!("Aborted."),
         Ok(Some(config)) => println!("{:#?}", config),
